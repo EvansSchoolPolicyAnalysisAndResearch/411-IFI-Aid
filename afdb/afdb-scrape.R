ed4ab987e5740921db966a5413d4c6fc2acb0139
Basil Hariri <basilhariri@gmail.com>
2021-07-16 09:59:03 -0700

    Whitespace changes

--- afdb/afdb-scrape.R
+++ afdb/afdb-scrape.R
@@ -36,33 +36,33 @@ output_file <- if(debug) "../data/afdb-test.csv" else "../data/afdb-data.csv"
 # From what i can tell this is the best way to prevent the whole system from 
 # crashing on a network glitch.
 get_html <- function(url) {
-	tryCatch(
-		read_html(url),
-		error = function(e) {
-			cat("request failed, retrying.\n")
-			Sys.sleep(5) 
-			NULL
-		}
-	)
+  tryCatch(
+    read_html(url),
+    error = function(e) {
+      cat("request failed, retrying.\n")
+      Sys.sleep(5) 
+      NULL
+    }
+  )
 }
 
 # a little bit of insurance against losing 13 hours of scraping by catching 
 # errors related to writing to disk. it happened. for the love of god don't 
 # try to go without error handling when the task takes 13 hours to run.
 write_file <- function(df, filename) {
-	tryCatch(
-		{
-			df %>% write.csv( 
-				file = filename, 
-				quote = TRUE)
-			TRUE
-	 	},
-		error = function(e) {
-			cat("writing to file failed, retrying.\n")
-			print(e)
-			FALSE
-		}
-	)
+  tryCatch(
+    {
+      df %>% write.csv( 
+        file = filename, 
+        quote = TRUE)
+      TRUE
+    },
+    error = function(e) {
+      cat("writing to file failed, retrying.\n")
+      print(e)
+      FALSE
+    }
+  )
 }
 
 main_table_parse <- function(page, x1) {
@@ -115,94 +115,94 @@ proj_ids <- read_lines(filename, skip_empty_rows = TRUE)
 # create a dataframe to hold the scraped data.
 data <- data.frame(matrix(ncol = 15, nrow = 0))
 names(data) <- c(
-		"Project Code",
-		"Country",
-		"Project Title",
-		"Objective",    
-		"Description",  
-		"Commitment in U.A.",
-		"Status",
-		"Start Date",
-		"Completion Date",
-    "Project Duration",
-		"Source of Financing",
-		"Sector",
-		"DAC Sector Code",
-		"DAC5 Code",
-		                #"DAC5 Description",
-		"Sovereign"
+  "Project Code",
+  "Country",
+  "Project Title",
+  "Objective",    
+  "Description",  
+  "Commitment in U.A.",
+  "Status",
+  "Start Date",
+  "Completion Date",
+  "Project Duration",
+  "Source of Financing",
+  "Sector",
+  "DAC Sector Code",
+  "DAC5 Code",
+                  #"DAC5 Description",
+  "Sovereign"
 )
 
 
 # for each id in provided file
 for(id in proj_ids) {
-	start <- Sys.time()
-	"scraping project" %>% paste(id, "\n", sep = " ") %>% cat()
-	page  <- NULL
-	count <- 0
+  start <- Sys.time()
+  "scraping project" %>% paste(id, "\n", sep = " ") %>% cat()
+  page  <- NULL
+  count <- 0
+  
+  # using a loop to retry on failed attempts just in case there is an http
+  # request error. only retry 20 times in case there is a bad id that doesn't
+  # work (it happened, don't remove the loop limiter)
+  while(length(page) == 0 && count < 20){
+    page <- get_html(paste(base_url, id, sep=""))
+    count <- count + 1
+  }
+  # if the loop ended because of the loop limiter skip to next id
+  if(length(page) == 0) next
 
-	# using a loop to retry on failed attempts just in case there is an http
-	# request error. only retry 20 times in case there is a bad id that doesn't
-	# work (it happened, don't remove the loop limiter)
-	while(length(page) == 0 && count < 20){
-	  page <- get_html(paste(base_url, id, sep=""))
-		count <- count + 1
-	}
-	# if the loop ended because of the loop limiter skip to next id
-	if(length(page) == 0) next
-
-	##############################
-	# parse the web page results #
-	##############################
-	# pull info from main web page table
-	funding <- all_table_parse(page, "Funding")
-	
-	commitment <- main_table_parse(page, "Commitment")
-	commitment <- if (str_length(commitment) > 5) substring(commitment, 5) else commitment
-	status <- main_table_parse(page, "Status")
-	approval_date <- main_table_parse(page, "Approval Date")
-	completion_date <- main_table_parse(page, "Planned Completion Date")
-	duration <- round(difftime(as.Date(completion_date, format="%d %b %Y"),
-	                           as.Date(approval_date, format="%d %b %Y"),
-	                           units="days")
+  ##############################
+  # parse the web page results #
+  ##############################
+  # pull info from main web page table
+  funding <- all_table_parse(page, "Funding")
+  
+  commitment <- main_table_parse(page, "Commitment")
+  commitment <- if (str_length(commitment) > 5) substring(commitment, 5) else commitment
+  status <- main_table_parse(page, "Status")
+  approval_date <- main_table_parse(page, "Approval Date")
+  completion_date <- main_table_parse(page, "Planned Completion Date")
+  duration <- round(difftime(as.Date(completion_date, format="%d %b %Y"),
+                             as.Date(approval_date, format="%d %b %Y"),
+                             units="days")
                     / 365.25, 2)
-	sector <- main_table_parse(page, "Sector")
-	dac <- main_table_parse(page, "DAC Sector Code")
-	dac5 = substring(dac, 1,3)
-	sov <- main_table_parse(page, "Sovereign / Non-Sovereign")
-	#print(paste("funding: ", funding))
+  sector <- main_table_parse(page, "Sector")
+  dac <- main_table_parse(page, "DAC Sector Code")
+  dac5 = substring(dac, 1,3)
+  sov <- main_table_parse(page, "Sovereign / Non-Sovereign")
+  #print(paste("funding: ", funding))
+  
+  #Get country & project names
+  country_plus_project <- str_split(page %>% html_node("h2") %>% html_text2(), " - ")[[1]]
+  country <- if(is.na(country_plus_project[1])) "N/A" else country_plus_project[1]
+  project <- if(is.na(country_plus_project[2])) "N/A" else country_plus_project[2]
+  
+  # by default set to n/a will replace later if we find what we're after
+  desc <- "N/A"
+  obj <- "N/A"
+  # pull out all the divs and put them in a vector like object
+  divs <- page %>% html_nodes("div")
+  # iterate through the list looking for information
+  for(div in divs) {
+    if(html_text(html_node(div,"h3")) %in% "Project General Description"){
+      desc <- div %>% html_node("p") %>% html_text()
+    }
+    if(html_text(html_node(div,"h3")) %in% "Project Objectives") {
+      obj <- div %>% html_node("p") %>% html_text()
+    }
+  }
 	
-	#Get country & project names
-	country_plus_project <- str_split(page %>% html_node("h2") %>% html_text2(), " - ")[[1]]
-	country <- if(is.na(country_plus_project[1])) "N/A" else country_plus_project[1]
-	project <- if(is.na(country_plus_project[2])) "N/A" else country_plus_project[2]
-
-	# by default set to n/a will replace later if we find what we're after
-	desc <- "N/A"
-	obj <- "N/A"
-	# pull out all the divs and put them in a vector like object
-	divs <- page %>% html_nodes("div")
-	# iterate through the list looking for information
-	for(div in divs) {
-		if(html_text(html_node(div,"h3")) %in% "Project General Description"){
-			desc <- div %>% html_node("p") %>% html_text()
-		}
-		if(html_text(html_node(div,"h3")) %in% "Project Objectives") {
-			obj <- div %>% html_node("p") %>% html_text()
-		}
-	}
-	
-	if(debug) print(paste(id, country, project, commitment, status, approval_date, completion_date, duration, funding, sector, dac, dac5, sov, sep="; "))
-
-	# add parsed data into the main data frame
-	data[nrow(data) + 1,] <- c(id, country, project, obj, desc, commitment, status, approval_date, completion_date, duration, funding, sector, dac, dac5, sov)
-
-	# calculate elapsed time for request
-	elapsed <- Sys.time() - start
-	# Respect the robots.txt - only do a request every 10 seconds
-	if(elapsed < 10) {
-		Sys.sleep(10-elapsed)
-	}
+  if(debug) print(paste(id, country, project, commitment, status, approval_date, completion_date, duration, funding, sector, dac, dac5, sov, sep="; "))
+  
+  # add parsed data into the main data frame
+  data[nrow(data) + 1,] <- c(id, country, project, obj, desc, commitment, status, approval_date, completion_date, duration, funding, sector, dac, dac5, sov)
+  
+  # calculate elapsed time for request
+  elapsed <- Sys.time() - start
+  # Respect the robots.txt - only do a request every 10 seconds
+  if(elapsed < 10) {
+    Sys.sleep(10-elapsed)
+  }
 }
 
 # Write to disk
