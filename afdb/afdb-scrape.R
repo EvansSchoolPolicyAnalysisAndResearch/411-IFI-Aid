3cf2c5310a6eb9a0311850fac694d3a5a487d9cf
Basil Hariri <bahariri@uw.edu>
2021-07-22 13:19:07 -0700

    Minor country/project parse fix, starting DAC lookup table

--- afdb/afdb-scrape.R
+++ afdb/afdb-scrape.R
@@ -16,39 +16,41 @@
 # outputs: a csv file containing project numbers, description, planned         #
 #          completion date, and dac sector codes                               #
 ################################################################################
-#install.packages("tidyverse")
-#install.packages("xml2")
-#install.packages("magrittr")
-#install.packages("rvest")
-#install.packages("sjmisc")
-library(tidyverse)
-library(xml2)
+
+# IMPORTS #
+# install.packages("tidyverse")
+# install.packages("xml2")
+# install.packages("magrittr")
+# install.packages("rvest")
+# install.packages("sjmisc")
 library(magrittr)
+library(readxl)
 library(rvest)
 library(sjmisc)
+library(tidyverse)
+library(xml2)
 
-debug <- FALSE
+# CONSTANTS #
+DEBUG <- TRUE
+BASE_URL <- "https://projectsportal.afdb.org/dataportal/VProject/show/"
+PROJECTS_FILE <- if(DEBUG) "afdb-short.txt" else "afdb-ids.txt"
+OUTPUT_FILE <- if(DEBUG) "../data/afdb_test.csv" else "../data/afdb_data.csv"
+DAC_FILE <- "../DAC-CRS-CODES.xls"
 
-base_url <- "https://projectsportal.afdb.org/dataportal/VProject/show/"
-filename <- if(debug) "afdb-short.txt" else "afdb-ids.txt"
-output_file <- if(debug) "../data/afdb_test.csv" else "../data/afdb_data.csv"
-
-# From what i can tell this is the best way to prevent the whole system from 
-# crashing on a network glitch.
+# FUNCTIONS #
+#Scrape
 get_html <- function(url) {
   tryCatch(
     read_html(url),
     error = function(e) {
-      cat("request failed, retrying.\n")
+      cat("Request failed, retrying.\n")
       Sys.sleep(5) 
       NULL
     }
   )
 }
 
-# a little bit of insurance against losing 13 hours of scraping by catching 
-# errors related to writing to disk. it happened. for the love of god don't 
-# try to go without error handling when the task takes 13 hours to run.
+# File write
 write_file <- function(df, filename) {
   tryCatch(
     {
@@ -65,18 +67,7 @@ write_file <- function(df, filename) {
   )
 }
 
-#Cleaner than all_table_parse, but also less generalizable
-# (only searches the first table in the HTML)
-main_table_parse <- function(page, x1) {
-  temp <- (page %>% 
-    html_node(".table") %>%
-    html_table(header = FALSE) %>%
-    filter(X1 == x1))
-  # check if table had requested data, if not replaced with "N/A"
-  return (if(nrow(temp) == 0) "N/A" else temp[['X2']])
-}
-
-#Searches every table in the html page for x1
+#Searches every table in the html page for x1's data
 all_table_parse <- function(page, x1) {
   ret <- ""
   tables <- page %>% html_nodes("table")
@@ -85,7 +76,7 @@ all_table_parse <- function(page, x1) {
     table <- tryCatch({
       tables %>% .[[i]] %>% html_table(header = FALSE, trim=TRUE)
     }, error = function(err){
-      if(debug) print(err)
+      if(DEBUG) print(err)
       NULL
     })
     
@@ -94,13 +85,13 @@ all_table_parse <- function(page, x1) {
     #Does current table have bad formatting?
     if(str_contains(table[['X1']], "\n\t") && !str_contains(table[['X1']], "Download")){
       #Correct formatting using regex (sorry)
-      table_str <- str_replace_all(table[['X1']], "[^\\S ]+[ ]+[^\\S ]+", "***")
-      table_list <- strsplit(table_str, "\\*\\*\\*")
+      table <- str_replace_all(table[['X1']], "[^\\S ]+[ ]+[^\\S ]+", "***")
+      table <- strsplit(table, "\\*\\*\\*")
       #Search through table rows for matches (i.e. filter)
-      if(!is.null(table_list) && length(table_list) > 0 && str_contains(table_list, x1)){
-        for(j in 1:length(table_list)){
-          if(str_contains(table_list[[j]][[1]], x1)){
-            ret <- paste(ret, table_list[[j]][[2]], sep = ", ")
+      if(!is.null(table) && length(table) > 0 && str_contains(table, x1)){
+        for(j in 1:length(table)){
+          if(str_contains(table[[j]][[1]], x1)){
+            ret <- paste(ret, table[[j]][[2]], sep = ", ")
           }  
         }
         #Return if we've found the indicator
@@ -125,8 +116,26 @@ all_table_parse <- function(page, x1) {
   return(NULL)
 }
 
-# grab the project ids from the file specified earlier in the file
-proj_ids <- read_lines(filename, skip_empty_rows = TRUE)
+#Cleaner than all_table_parse, but less generalizable
+# (only searches the first table in the HTML)
+main_table_parse <- function(page, x1) {
+  temp <- (page %>% 
+             html_node(".table") %>%
+             html_table(header = FALSE) %>%
+             filter(X1 == x1))
+  # check if table had requested data, if not replace with "N/A"
+  return (if(nrow(temp) == 0) "N/A" else temp[['X2']])
+}
+
+#Looks up the DAC or DAC5 code in the DAC-CRS-CODES excel file
+get_dac5_desc <- function(dac_code){
+  print(dac_lookup_df)
+  return("TODO")
+}
+
+# MAIN #
+proj_ids <- read_lines(PROJECTS_FILE, skip_empty_rows = TRUE)
+dac_lookup_df <- read_excel(DAC_FILE, sheet=12, skip=2)
 
 # create a dataframe to hold the scraped data.
 data <- data.frame(matrix(ncol = 17, nrow = 0))
@@ -163,7 +172,7 @@ for(id in proj_ids) {
   # request error. only retry 20 times in case there is a bad id that doesn't
   # work (it happened, don't remove the loop limiter)
   while(length(page) == 0 && count < 20){
-    page <- get_html(paste(base_url, id, sep=""))
+    page <- get_html(paste(BASE_URL, id, sep=""))
     count <- count + 1
   }
   # if the loop ended because of the loop limiter skip to next id
@@ -173,8 +182,9 @@ for(id in proj_ids) {
   # parse the web page results #
   ##############################
   # pull info from main web page table, only look at active projects
-  if(main_table_parse(page, "Status") != "Implementation"){
-    if(debug) print(paste("Skipping inactive project:", id))
+  status <- main_table_parse(page, "Status")
+  if(status != "Implementation" && status != "Approved"){
+    if(DEBUG) print(paste("Skipping inactive project:", id))
     next
   } 
   
@@ -192,8 +202,8 @@ for(id in proj_ids) {
   sector <- main_table_parse(page, "Sector")
   dac <- main_table_parse(page, "DAC Sector Code")
   dac5 = substring(dac, 1,3)
-  dac5_desc = "TODO"
-  dac5_desc_detailed = "TODO"
+  dac5_desc <- get_dac5_desc(dac5)
+  dac5_desc_detailed <- get_dac5_desc(dac)
   
   contact_name <- all_table_parse(page, "Name")
   contact_name <- if (!is.null(contact_name)) str_to_title(contact_name) else "N/A"
@@ -202,10 +212,17 @@ for(id in proj_ids) {
   funding <- all_table_parse(page, "Funding")
   
   #Get country & project names
-  country_plus_project <- str_split(page %>% html_node("h2") %>% html_text2(), " - ")[[1]]
+  country_plus_project <- page %>% html_node("h2") %>% html_text2()
+  country_plus_project <- str_replace(country_plus_project, " ?[\\p{Pd}|:] ?", " - ")
+  country_plus_project <- str_split(country_plus_project, " - ", n=2)[[1]]
   country <- if(is.na(country_plus_project[1])) "N/A" else country_plus_project[1]
   project <- if(is.na(country_plus_project[2])) "N/A" else country_plus_project[2]
-  
+  #If the country exists and the project doesn't, that usually means that the project name was not of the form "country - project"
+  if(country != "N/A" && project == "N/A"){
+    project <- country
+    country <- "N/A"
+  }
+
   # by default set to n/a will replace later if we find what we're after
   desc <- ""
   # pull out all the divs and put them in a vector like object
@@ -220,7 +237,7 @@ for(id in proj_ids) {
     }
   }
   
-  if(debug) print(paste(id, country, project, commitment, approval_date, completion_date, duration, funding, sector, sov, dac, dac5, dac5_desc, dac5_desc_detailed, contact_name, contact_email, sep="; "))
+  if(DEBUG) print(paste(id, country, project, commitment, approval_date, completion_date, duration, funding, sector, sov, dac, dac5, dac5_desc, dac5_desc_detailed, contact_name, contact_email, sep="; "))
   # add parsed data into the main data frame
   data[nrow(data) + 1,] <- c(id, country, project, desc, commitment, approval_date, completion_date, duration, funding, sector, sov, dac, dac5, dac5_desc, dac5_desc_detailed, contact_name, contact_email)
   
@@ -236,7 +253,7 @@ for(id in proj_ids) {
 written <- FALSE
 count <- 0
 while(!written && count < 20){
-	written <- write_file(data, output_file)
+	written <- write_file(data, OUTPUT_FILE)
 	count <- count + 1
 }
 
